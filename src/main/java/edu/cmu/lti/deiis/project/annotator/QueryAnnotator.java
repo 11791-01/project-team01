52588fc71241a7afe439acd86bb1f1c73cec1569
KyleMao <maozeximzx@gmail.com>
2014-11-08 18:05:38 -0500

    Fixed QueryAnnotator

--- src/main/java/edu/cmu/lti/deiis/project/annotator/QueryAnnotator.java
+++ src/main/java/edu/cmu/lti/deiis/project/annotator/QueryAnnotator.java
@@ -34,10 +34,9 @@ public class QueryAnnotator extends JCasAnnotator_ImplBase {
     if (iter.isValid() && iter.hasNext()) {
       Question question = (Question) iter.next();
       
-      System.out.println(question.getText().replace("?", ""));
-      
       AtomicQueryConcept atomicQuery = new AtomicQueryConcept(aJCas);
-      atomicQuery.setText(question.getText());
+      atomicQuery.setText(question.getText().replace("?", ""));
+      System.out.println(atomicQuery.getText());
       atomicQuery.addToIndexes();
       List<AtomicQueryConcept> terms = new ArrayList<AtomicQueryConcept>();
       terms.add(atomicQuery);
