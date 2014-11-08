16b63a762c1f240f09d96105ae9dac9bae119978
Fei Xia <feixia@cs.cmu.edu>
2014-11-08 15:20:59 -0500

    add query triple annotator

---
+++ src/main/java/edu/cmu/lti/deiis/project/annotator/QueryTriple.java
@@ -0,0 +1,55 @@
+package edu.cmu.lti.deiis.project.annotator;
+
+import java.util.Iterator;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIndex;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import edu.cmu.lti.oaqa.bio.bioasq.services.GoPubMedService;
+import edu.cmu.lti.oaqa.bio.bioasq.services.OntologyServiceResponse;
+import edu.cmu.lti.oaqa.type.input.Question;
+
+public class QueryTriple extends JCasAnnotator_ImplBase {
+
+  GoPubMedService service;
+
+  /**
+   * Perform initialization logic. Initialize the service.
+   * 
+   * @param aContext
+   *          the UimaContext object
+   */
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    super.initialize(aContext);
+
+    try {
+      service = new GoPubMedService("project.properties");
+    } catch (Exception ex) {
+      throw new ResourceInitializationException();
+    }
+  }
+
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    FSIndex<?> QuestionIndex = aJCas.getAnnotationIndex(Question.type);
+    Iterator<?> QuestionIter = QuestionIndex.iterator();
+
+    try {
+      Question question = (Question) QuestionIter.next();
+
+      String text = question.getText();
+      OntologyServiceResponse.Result meshResult = service.findMeshEntitiesPaged(text, 0);
+      System.out.println("MeSH: " + meshResult.getFindings().size());
+      for (OntologyServiceResponse.Finding finding : meshResult.getFindings()) {
+        System.out.println(" > " + finding.getConcept().getLabel() + " "
+                + finding.getConcept().getUri());
+      }
+    } catch (Exception ex) {
+
+    }
+  }
+}
