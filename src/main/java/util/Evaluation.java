c4bb18516c3e0514c04441124d5ee5a7f15d38a9
Fei Xia <feixia@cs.cmu.edu>
2014-11-10 14:24:14 -0500

    add methods to Evaluation.java

--- src/main/java/util/Evaluation.java
+++ src/main/java/util/Evaluation.java
@@ -1,5 +1,66 @@
 package util;
 
-public class Evaluation {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class Evaluation {
+  private <T> double precision(List<T> trueval, List<T> retval) {
+
+    Set<T> trueset = new HashSet<T>(trueval);
+    Set<T> retset = new HashSet<T>(retval);
+
+    retset.retainAll(trueset);
+
+    Integer TP = retset.size();
+
+    if (retval.size() == 0) {
+      return 0;
+    }
+    return ((double) TP) / ((double) retval.size());
+
+  }
+
+  private <T> double recall(List<T> trueval, List<T> retval) {
+
+    Set<T> trueset = new HashSet<T>(trueval);
+    Set<T> retset = new HashSet<T>(retval);
+
+    retset.retainAll(trueset);
+
+    Integer TP = retset.size();
+
+    if (trueval.size() == 0) {
+      return 0;
+    }
+    return ((double) TP) / ((double) trueval.size());
+
+  }
+
+  private double fmeasure(Double prec, Double rec) {
+
+    if (prec + rec == 0) {
+      return 0;
+    }
+    return (2 * prec * rec) / (prec + rec);
+
+  }
+
+  private <T> Double AP(List<T> trueval, List<T> retval) {
+
+    int poscount = 0;
+    Double ap = 0.0;
+    int c = 0;
+    for (T item : retval) {
+
+      if (trueval.contains(item)) {
+        poscount += 1;
+        ap += (poscount / ((double) (c + 1)));
+      }
+      c = c + 1;
+    }
+
+    return ap / (poscount + Math.pow(10, -15));
+
+  }
 }
